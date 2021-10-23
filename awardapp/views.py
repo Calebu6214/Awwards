c01b858b35fddf1c803d6d57d6b239e660a12e61
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-23 10:59:12 +0300

    add new project function

--- awardapp/views.py
+++ awardapp/views.py
@@ -63,3 +63,18 @@ def get_project_by_id(request, id):
         raise Http404()    
     
     return render(request, "project.html", {"project":project})
+
+@login_required(login_url='login/')
+def new_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.author = current_user
+            form.save()
+        return redirect('/')
+
+    else:
+        form = NewProjectForm()
+    return render(request, 'new_project.html', {"form":form})
