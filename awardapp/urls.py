e867ba70b5b8f272e024b2438bd6e7e32c818b51
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-23 11:51:28 +0300

    add urls

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path,re_path,include
+from django.conf.urls import include, url
+from django.urls.conf import re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home, name='home'),
+    re_path('project/(\d+)', views.get_project_by_id, name='project_results'),
+    path('new/project', views.new_project, name='new-project'),
+    path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('search/', views.search_projects, name='search_results'),
+    path('accounts/profile/', views.user_profiles, name='profile'),
     path('registration/', views.registerPage, name="registration"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
