818a1da12f89f8acda59d7b33532cf4f71c0292a
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-23 11:01:58 +0300

    add search function

--- awardapp/views.py
+++ awardapp/views.py
@@ -78,3 +78,16 @@ def new_project(request):
     else:
         form = NewProjectForm()
     return render(request, 'new_project.html', {"form":form})
+
+@login_required(login_url='login/')
+def search_projects(request):
+    if 'keyword' in request.GET and request.GET["keyword"]:
+        search_term = request.GET.get("keyword")
+        searched_projects = Projects.search_projects(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
