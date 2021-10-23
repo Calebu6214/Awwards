17792dceb91ab4df423b0c825ff8623b15bdb507
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-10-23 12:14:04 +0300

    api urls

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('',views.home, name='home'),
     re_path('project/(\d+)', views.get_project_by_id, name='project_results'),
     path('new/project', views.new_project, name='new-project'),
+    path('api/projects/', views.ProjectsList.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
     path('search/', views.search_projects, name='search_results'),
     path('accounts/profile/', views.user_profiles, name='profile'),
